35ccd9dab1da4bc3baae29fcd61c1dfa0b4dd55f
malu <malu@malu.me>
2020-04-26 15:02:50 +0800

    autopush

--- src/main/java/me/malu/mtool/JR.java
+++ src/main/java/me/malu/mtool/JR.java
@@ -8,50 +8,48 @@ import java.io.Serializable;
  * @author malu
  * @since 2020-04-07
  */
-public class JsonResult<T> implements Serializable {
+public class JR<T> implements Serializable {
     //Serializable将对象的状态保存在存储媒体中以便可以在以后重新创建出完全相同的副本
     public static final int SUCCESS = 0;
     public static final int ERROR = 1;
-    public static final int OTHER = 2;
 
     private int state;
     private String message = "";
     private T data;
-    private String pass = "";
 
-    public JsonResult() {
+    public JR() {
         state = SUCCESS;
     }
 
     //为了方便，重载n个构造器
-    public JsonResult(int state, String message, T data) {
+    public JR(int state, String message, T data) {
         super();
         this.state = state;
         this.message = message;
         this.data = data;
     }
 
-    public JsonResult(int state, String error) {
+    public JR(int state, String error) {
         this(state, error, null);
     }
 
-    public JsonResult(int state, T data) {
+    public JR(int state, T data) {
         this(state, "", data);
     }
 
-    public JsonResult(String error) {
+    public JR(String error) {
         this(ERROR, error, null);
     }
 
-    public JsonResult(T data) {
+    public JR(T data) {
         this(SUCCESS, "", data);
     }
 
-    public JsonResult(int state) {
+    public JR(int state) {
         this(state, "", null);
     }
 
-    public JsonResult(Throwable e) {
+    public JR(Throwable e) {
         this(ERROR, e.getMessage(), null);
     }
 
@@ -79,14 +77,6 @@ public class JsonResult<T> implements Serializable {
         this.data = data;
     }
 
-//    public String getDebug_info() {
-//        return debug_info;
-//    }
-
-//    public void setDebug_info(String debug_info) {
-//        this.debug_info = debug_info;
-//    }
-
     public static int getSuccess() {
         return SUCCESS;
     }
@@ -97,10 +87,6 @@ public class JsonResult<T> implements Serializable {
 
     @Override
     public String toString() {
-//        if (debug_info.equals("")) {
-        return "JsonResult [state=" + state + ", message=" + message + ", pass=" + pass + ", data=" + data + "]";
-//        } else {
-//        return "JsonResult [state=" + state + ", message=" + message + ", pass=" + pass + ", debug_info=" + debug_info + ", data=" + data + "]";
-//        }
+        return "JR [state=" + state + ", message=" + message + ", data=" + data + "]";
     }
 }
--- src/main/java/me/malu/mtool/mJson.java
+++ src/main/java/me/malu/mtool/mJson.java
@@ -3,6 +3,9 @@ package me.malu.mtool;
 /**
  * 用静态方法减少代码量
  *
+ * v1.* 返回值对象JsonResult
+ * v2.* 返回值对象JR
+ *
  * @author malu
  * @since 2020-04-07
  */
@@ -10,22 +13,22 @@ public final class mJson {
     public static boolean debug = false;
     public static String success_msg = "成功";
 
-    public static class JR extends JsonResult {
+    public static class JR_debug extends JR {
         public static final int SUCCESS = 0;
         public static final int ERROR = 1;
         String debug_info;
 
-        public JR(String debug_info) {
+        public JR_debug(String debug_info) {
             super(SUCCESS, "成功", null);
             this.debug_info = debug_info;
         }
 
-        public JR(int state, String message, Object data, String debug_info) {
+        public JR_debug(int state, String message, Object data, String debug_info) {
             super(state, message, data);
             this.debug_info = debug_info;
         }
 
-        public JR(Throwable e, String debug_info) {
+        public JR_debug(Throwable e, String debug_info) {
             super(ERROR, e.getMessage(), null);
             this.debug_info = debug_info;
         }
@@ -37,47 +40,45 @@ public final class mJson {
         public void setDebug_info(String debug_info) {
             this.debug_info = debug_info;
         }
-
-
     }
 
-    public static <T> JsonResult mJson(int state, String message, T data) {
+    public static <T> JR mJson(int state, String message, T data) {
         if (debug) {
             Thread currentThread = Thread.currentThread();
             StackTraceElement stackTrace = currentThread.getStackTrace()[2];
-            return new JR(state, message, data, stackTrace.getFileName() + ":" + stackTrace.getLineNumber());
+            return new JR_debug(state, message, data, stackTrace.getFileName() + ":" + stackTrace.getLineNumber());
         } else {
-            return new JsonResult(state, message, data);
+            return new JR(state, message, data);
         }
     }
 
-    public static JsonResult mJson() {
+    public static JR mJson() {
         if (debug) {
             Thread currentThread = Thread.currentThread();
             StackTraceElement stackTrace = currentThread.getStackTrace()[2];
-            return new JR(stackTrace.getFileName() + ":" + stackTrace.getLineNumber());
+            return new JR_debug(stackTrace.getFileName() + ":" + stackTrace.getLineNumber());
         } else {
-            return new JsonResult();
+            return new JR();
         }
     }
 
-    public static JsonResult mJson(Throwable e) {
+    public static JR mJson(Throwable e) {
         if (debug) {
             Thread currentThread = Thread.currentThread();
             StackTraceElement stackTrace = currentThread.getStackTrace()[2];
-            return new JR(e, stackTrace.getFileName() + ":" + stackTrace.getLineNumber());
+            return new JR_debug(e, stackTrace.getFileName() + ":" + stackTrace.getLineNumber());
         } else {
-            return new JsonResult(e);
+            return new JR(e);
         }
     }
 
-    public static <T> JsonResult mJson(T data) {
+    public static <T> JR mJson(T data) {
         if (debug) {
             Thread currentThread = Thread.currentThread();
             StackTraceElement stackTrace = currentThread.getStackTrace()[2];
-            return new JR(1, success_msg, data, stackTrace.getFileName() + ":" + stackTrace.getLineNumber());
+            return new JR_debug(1, success_msg, data, stackTrace.getFileName() + ":" + stackTrace.getLineNumber());
         } else {
-            return new JsonResult(1, success_msg, data);
+            return new JR(1, success_msg, data);
         }
     }
 }
